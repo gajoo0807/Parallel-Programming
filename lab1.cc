4d4d264ea0905872b19394eca74ec933557c5f40
gajoo0807 <ab95113@gmail.com>
2023-10-01 13:24:21 +0800

    Determine whether overflow

--- lab1.cc
+++ lab1.cc
@@ -1,25 +1,5 @@
-// #include <assert.h>
-// #include <stdio.h>
-// #include <math.h>
-
-// int main(int argc, char** argv) {
-// 	if (argc != 3) {
-// 		fprintf(stderr, "must provide exactly 2 arguments!\n");
-// 		return 1;
-// 	}
-// 	unsigned long long r = atoll(argv[1]);
-// 	unsigned long long k = atoll(argv[2]);
-// 	unsigned long long pixels = 0;
-// 	for (unsigned long long x = 0; x < r; x++) {
-// 		unsigned long long y = ceil(sqrtl(r*r - x*x));
-// 		pixels += y;
-// 		pixels %= k;
-// 	}
-// 	printf("%llu\n", (4 * pixels) % k);
-// }
 #include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
 int main(int argc, char *argv[]) {
@@ -41,10 +21,10 @@ int main(int argc, char *argv[]) {
     for (unsigned long x = start_x; x < end_x; x++) {
         unsigned long long y = ceil(sqrtl(r * r - x * x));
         pixels += y;
-        if(x % 30 == 0){
+        if(pixels + y < pixels) // overflow
             pixels %= k;
-        }
     }
+    pixels %= k;
 
     // Collect the 'pixels' values from all processes
     unsigned long long global_pixels = 0;
